c2a62d14ff90570103ff5fd5ad8daaeef3f083c5
Alfredo <alfredo.perez@blanclink.com>
2015-10-23 09:50:01 -0430

    Initial Commit

---
+++ APDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APDatePicker
+//
+//  Created by Alfredo Perez on 10/23/15.
+//  Copyright © 2015 Alfredo Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APDatePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APDatePicker
+//
+//  Created by Alfredo Perez on 10/23/15.
+//  Copyright © 2015 Alfredo Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
